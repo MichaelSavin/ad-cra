cf3ba075d97a72ebda99807290e20b32d42044e6
Michael Savin <m.savin.ru@gmail.com>
2019-10-18 00:54:12 +0500

    setFieldValue custom inputs support

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Formik} from 'formik';
 import * as Yup from "yup";
 import './App.css';
 
+// TODO Переписать с применением массива
 const {Option} = Select;
 
 const App = () => (
@@ -11,9 +12,9 @@ const App = () => (
         initialValues={{
             numberOfRockets: 3,
             isAllowed: true,
+            fuelingPercentage: 80,
             email: '',
             password: '',
-            defaultFuelingPercentage: 80,
         }}
         validationSchema={Yup.object({
             numberOfRockets: Yup.number()
@@ -44,110 +45,114 @@ const App = () => (
               handleChange,
               handleBlur,
               handleSubmit,
-          }) => (
-            <Form onSubmit={handleSubmit} style={{marginTop: 32}}>
-                <Form.Item
-                    label="Сколько запустить ракет?"
-                    labelCol={{span: 8}}
-                    wrapperCol={{span: 8}}
-                >
-                    <InputNumber
-                        name="numberOfRockets"
-                        onChange={handleChange}
+              setFieldValue,
+          }) => <Form onSubmit={handleSubmit} style={{marginTop: 32}}>
+            <Form.Item
+                label="Сколько запустить ракет?"
+                labelCol={{span: 8}}
+                wrapperCol={{span: 8}}
+            >
+                <InputNumber
+                    name="numberOfRockets"
+                    onChange={e => setFieldValue('numberOfRockets', e)}
+                    onBlur={handleBlur}
+                    defaultValue={values.numberOfRockets}
+                    min={1}
+                    max={10}
+                />
+                <span className="ant-form-text">или включить</span>
+                <a href="https://ant.design">План Б</a>
+                <br></br>
+                {errors.numberOfRockets && touched.numberOfRockets && errors.numberOfRockets}
+            </Form.Item>
+
+            <Form.Item
+                label="Запуск разрешен"
+                labelCol={{span: 8}}
+                wrapperCol={{span: 8}}
+            >
+                <Switch defaultChecked
+                        name="isAllowed"
+                        onChange={e => setFieldValue('isAllowed', e)}
                         onBlur={handleBlur}
-                        value={values.numberOfRockets} //FIXME эта строка блокирует изменение количества, а без неё я не знаю как вернуть значение из формы ((
-                    />
-                    <span className="ant-form-text">или включить</span>
-                    <a href="https://ant.design">План Б</a>
-                    {errors.numberOfRockets && touched.numberOfRockets && errors.numberOfRockets}
-                </Form.Item>
+                />
+                <br></br>
+                {errors.isAllowed && touched.isAllowed && errors.isAllowed}
+            </Form.Item>
 
-                <Form.Item
-                    label="Запуск разрешен"
-                    labelCol={{span: 8}}
-                    wrapperCol={{span: 8}}
-                >
-                    <Switch defaultChecked
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        checked={values.isAllowed} //FIXME эта строка блокирует изменение флага, а без неё я не знаю как вернуть состояние флага
-                    />
-                </Form.Item>
+            <Form.Item
+                label="Заправлено топлива"
+                labelCol={{span: 8}}
+                wrapperCol={{span: 8}}
+            >
+                <Slider
+                    name="fuelingPercentage"
+                    onChange={e => setFieldValue('fuelingPercentage', e)}
+                    onBlur={handleBlur}
+                    defaultValue={values.fuelingPercentage}
+                />
+            </Form.Item>
 
-                <Form.Item
-                    label="Заправлено топлива"
-                    labelCol={{span: 8}}
-                    wrapperCol={{span: 8}}
-                >
-                    <Slider
-                        name="fuelingPercentage"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        defaultValue={values.defaultFuelingPercentage}
-                    />
-                </Form.Item>
+            <Form.Item
+                label="Командир"
+                labelCol={{span: 8}}
+                wrapperCol={{span: 8}}
+            >
+                <Select defaultValue="lucy" style={{width: 192}}>
+                    <Option value="jack">Лёша</Option>
+                    <Option value="lucy">Люся</Option>
+                    <Option value="disabled" disabled>выбыл</Option>
+                    <Option value="mick">Сан Саныч</Option>
+                </Select>
+            </Form.Item>
 
-                <Form.Item
-                    label="Командир"
-                    labelCol={{span: 8}}
-                    wrapperCol={{span: 8}}
-                >
-                    <Select defaultValue="lucy" style={{width: 192}}>
-                        <Option value="jack">Лёша</Option>
-                        <Option value="lucy">Люся</Option>
-                        <Option value="disabled" disabled>выбыл</Option>
-                        <Option value="yiminghe">Сан Саныч</Option>
-                    </Select>
-                </Form.Item>
+            <Form.Item
+                label="Дата запуска"
+                labelCol={{span: 8}}
+                wrapperCol={{span: 8}}
+            >
+                <DatePicker/>
+            </Form.Item>
 
-                <Form.Item
-                    label="Дата запуска"
-                    labelCol={{span: 8}}
-                    wrapperCol={{span: 8}}
-                >
-                    <DatePicker/>
-                </Form.Item>
+            <Form.Item
+                label="e-mail"
+                labelCol={{span: 8}}
+                wrapperCol={{span: 8}}
+            >
+                <Input
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                />
+                {errors.email && touched.email && errors.email}
+            </Form.Item>
 
-                <Form.Item
-                    label="e-mail"
-                    labelCol={{span: 8}}
-                    wrapperCol={{span: 8}}
-                >
-                    <Input
-                        type="email"
-                        name="email"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.email}
-                    />
-                    {errors.email && touched.email && errors.email}
-                </Form.Item>
+            <Form.Item
+                label="Пароль"
+                labelCol={{span: 8}}
+                wrapperCol={{span: 8}}
+            >
+                <Input
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                />
+                {errors.password && touched.password && errors.password}
+            </Form.Item>
 
-                <Form.Item
-                    label="Пароль"
-                    labelCol={{span: 8}}
-                    wrapperCol={{span: 8}}
-                >
-                    <Input
-                        type="password"
-                        name="password"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.password}
-                    />
-                    {errors.password && touched.password && errors.password}
-                </Form.Item>
-
-                <Form.Item wrapperCol={{span: 8, offset: 8}}>
-                    <Button type="primary" htmlType="submit">
-                        Подтвердить полномочия
-                    </Button>
-                    <Button style={{marginLeft: 8}}>
-                        Отмена
-                    </Button>
-                </Form.Item>
-            </Form>
-        )}
+            <Form.Item wrapperCol={{span: 8, offset: 8}}>
+                <Button type="primary" htmlType="submit">
+                    Подтвердить полномочия
+                </Button>
+                <Button style={{marginLeft: 8}}>
+                    Отмена
+                </Button>
+            </Form.Item>
+        </Form>}
     </Formik>
 );
 
